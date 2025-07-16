748a41296c66bbf307cd18fc3753bbda5143fae4
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-07-16 17:50:03 +0200

    🏷️  Add type stubs for pymemcache client and related modules
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

---
+++ stubs/pymemcache/__init__.pyi
@@ -0,0 +1,5 @@
+from pymemcache.client.base import Client, KeepaliveOpts, PooledClient
+from pymemcache.client.hash import HashClient
+from pymemcache.exceptions import MemcacheClientError, MemcacheError, MemcacheIllegalInputError, MemcacheServerError, MemcacheUnexpectedCloseError, MemcacheUnknownCommandError, MemcacheUnknownError
+
+__version__ = ...
